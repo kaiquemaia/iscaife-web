f86956605c8d028c552163243f01dcacec8449ea
Kaique <kaiquefeng1@gmail.com>
2019-11-26 00:39:08 -0300

    customization files / includes

---
+++ inc/footer.php
@@ -0,0 +1,45 @@
+<footer id="footer" class="light-bg pad-top-xs pad-bottom-xs">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-4">
+						<!-- footer logo -->
+						<div class="logo pull-left">
+							<a href="home.html">
+								<img src="assets/images/iscaife-logo.png" alt="swam lake" class="img-responsive" width="150px">
+							</a>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-8">
+						<!-- top list -->
+						<ul class="list-inline text-right top-list">
+							<li>
+                            <i class="fa fa-envelope "></i>
+								<strong>E-mail:</strong>
+								<a href="mailto:Mail@Example.com">iscaifeboni@yahoo.com.br</a>
+							</li>
+							<li>
+								<i class="fa fa-phone "></i>
+								<strong>Contato:</strong>
+								<a href="tel:1123062848">(011) 2306-2848 </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<!-- footer bottom container -->
+					<div class="col-xs-12 col-sm-6 bottom">
+						<p>2019 © Dr. Alexandre | Todos os direitos reservados | Desenvolvido por <a href="http://380volts.com.br"> 380 Volts </a></p>
+					</div>
+					<!-- footer bottom container -->
+					<div class="col-xs-12 col-sm-6 bottom">
+						<ul class="list-inline text-right mt-social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-behance"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
---
+++ inc/header.php
@@ -0,0 +1,49 @@
+<header id="header" class="light-bg" data-scroll-index="0">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-4">
+						<!-- header logo -->
+						<div class="logo pull-left">
+							<a href="home.html">
+								<img src="assets/images/iscaife-logo.png" alt="swam lake" class="img-responsive" width="150px">
+							</a>
+						</div>
+						<a href="#" class="nav-opener pull-right"><i class="fa fa-bars" aria-hidden="true"></i></a>
+					</div>
+					<div class="col-xs-12 col-sm-8">
+						<!-- top list -->
+						<ul class="list-inline text-right top-list">
+							<li>
+								<i class="fa fa-envelope"></i>
+								<strong>E-mail:</strong>
+								<a href="mailto:Mail@Example.com">iscaifeboni@yahoo.com.br</a>
+							</li>
+							<li>
+								<i class="fa fa-phone "></i>
+								<strong>Contato:</strong>
+								<a href="tel:1123062848">(011) 2306-2848 </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- page navigation start here -->
+			<nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<ul class="nav-list list-inline">
+								<li><a href="#" data-scroll-nav="0" class="smooth">Home</a></li>
+                                <li><a href="#" data-scroll-nav="2" class="smooth">Sobre o Dr.</a></li>
+                                <li><a href="#" data-scroll-nav="3" class="nav-link dropdown-toggle">Cirurgias</a></li>
+								<li><a href="#" data-scroll-nav="4" class="smooth">Doenças</a></li>
+								<li><a href="mailto:Mail@Example.com">Contato</a></li>
+								<li class="button"><a href="#" class="btn main-bg-color text-uppercase smooth" data-scroll-nav="1">Orçamento</a></li>
+                            </ul>
+                            
+						</div>
+					</div>
+				</div>
+			</nav>
+			<!-- page navigation end here -->
+		</header>
