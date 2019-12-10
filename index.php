d6c44c98d5cd0df87b11a2bf124b628eb859d05c
kaiquemaia <kaiquefeng1@gmail.com>
2019-12-10 15:39:50 -0300

    modification css page index

--- index.php
+++ index.php
@@ -85,6 +85,7 @@
 											<input type="tel" id="phone" placeholder="Seu Telefone" class="form-control" required data-error="NEW ERROR MESSAGE">
 										</div>
 										<div class="form-group">
+											<p>Data da consulta:</p>
 											<input type="date" id="date" placeholder="Data" class="form-control" required data-error="NEW ERROR MESSAGE">
 										</div>
 										<div id="msgSubmit" class="form-message hidden"></div>
@@ -313,7 +314,20 @@
 			<!-- background-image: url(http://placehold.it/1920x1080);" -->
 
 			<!-- Section ( Agende sua consulta agora!) -->
-			<?php require_once("inc/agendar.php") ?>
+			<section class="bg-img-full bg-img-parallax quote-section pad-top-lg pad-bottom-lg" style="background-image: url(assets/images/clinica/7.jpeg);">
+				<span class="overlay"></span>
+				<div class="container">
+					<div class="row">
+						<div class="col-cs-12 col-sm-10 col-lg-10 col-sm-push-1 col-lg-push-2 text-center">
+						
+							<h2 class="main-color text-uppercase">Agende sua consulta Agora!</h2>
+							<!-- <h3 class="main-color text-uppercase">Com um especialista</h3> -->
+							<span class="divider white center"></span>
+							<a href="#popup1" class="order text-uppercase lightbox px-4"><button class="btn btn-default main-bg-color text-uppercase smooth">Agendar</button></a>
+						</div>
+					</div>
+				</div>
+			</section>
 
 		</main>
 		
